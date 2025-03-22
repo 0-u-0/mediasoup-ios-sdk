f9782a56da9bd1eeb254a259bd0ea1165ebe4d4d
craterone <craterone11@gmail.com>
2025-03-22 23:54:21 +0800

    Initial Commit

---
+++ sdkTests/sdkTests.swift
@@ -0,0 +1,17 @@
+//
+//  sdkTests.swift
+//  sdkTests
+//
+//  Created by cong chen on 2025/3/22.
+//
+
+import Testing
+@testable import sdk
+
+struct sdkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
